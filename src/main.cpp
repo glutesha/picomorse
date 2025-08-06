ae0f91f6a20cba8676581572a82cc0af88d8ed73
glutesha <mail@glutesha.me>
2025-08-06 15:28:03 +0300

    USB stack

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,30 @@
 #include <Arduino.h>
+#include "FastLED.h"
+
 #include "PluggableUSBHID.h"
-#include "USBKeyboard.h"
 #include "USBMouse.h"
 
-USBKeyboard keyboard;
 USBMouse mouse;
 
-void setup() {
-  pinMode(2, INPUT_PULLUP);
-  pinMode(28, OUTPUT);
-}
+CRGB leds[1];
 
+void setup() {
+  Serial.begin(9600);
+  pinMode(2, INPUT_PULLUP);
+  FastLED.addLeds<NEOPIXEL, 16>(leds, 1);
+}
+ 
 void loop() {
   if(!digitalRead(2)){
     mouse.press(MOUSE_LEFT);
+    tone(28, 1000, 8);
     Serial.println("PRESS");
+    leds[0] = CRGB::Blue;
   }
   else{
+    leds[0] = CRGB::Black;
+    noTone(28);
     mouse.release(MOUSE_LEFT);
   }
+  FastLED.show(); 
 }
